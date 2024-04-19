ef913a06d2e81ec4efa7d38a3b734eb1d8fc042e
Jefferson Silva <jefessh@outlook.com>
2024-04-19 09:09:57 -0300

    cria função para ler vértice o o maior grau

--- main.c
+++ main.c
@@ -2,6 +2,31 @@
 
 #define N 10  // Número de vértices no grafo
 
+// Função para encontrar o vértice com o maior grau
+void encontrarMaiorGrau(int grafo[N][N]) {
+    int maxGrau = 0;
+    int grau[N] = {0};
+
+    // Calcula o grau de cada vértice
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            grau[i] += grafo[i][j];
+        }
+        if (grau[i] > maxGrau) {
+            maxGrau = grau[i];
+        }
+    }
+
+    // Imprime os vértices com o maior grau
+    printf("Vertice(s) com o maior grau: \n");
+    for (int i = 0; i < N; i++) {
+        if (grau[i] == maxGrau) {
+            printf("d(%d) = %d\n", i+1, maxGrau);
+        }
+    }
+    printf("\n");
+}
+
 int main() {
     int grafo[N][N];
     FILE *file = fopen("matriz_teste.txt", "r");
@@ -19,13 +44,8 @@ int main() {
 
     fclose(file);
 
-    // Imprime a matriz para verificar se a leitura do arquivo está correta
-    // for (int i = 0; i < N; i++) {
-    //     for (int j = 0; j < N; j++) {
-    //         printf("%d ", grafo[i][j]);
-    //     }
-    //     printf("\n");
-    // }
+    // Chama a função para encontrar o vértice com o maior grau
+    encontrarMaiorGrau(grafo);
 
     return 0;
 }
