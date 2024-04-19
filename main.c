63f4dacd8bde53861aeceade45dbb5c3f2b1876f
Jefferson Silva <jefessh@outlook.com>
2024-04-18 22:56:42 -0300

    cria função para ler o arquivo: dados_matriz.txt

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+#define MAX 3633
 
-    return 0;
+int grafo[MAX][MAX];
+
+void lerGrafo() {
+    FILE *file = fopen("dados_matriz.txt", "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    for(int i=0; i<MAX; i++) {
+        for(int j=0; j<MAX; j++) {
+            if (fscanf(file, "%d", &grafo[i][j]) != 1) {
+                printf("Erro ao ler os dados na linha %d, coluna %d\n", i+1, j+1);
+                return;
+            }
+        }
+    }
+    fclose(file);
 }
